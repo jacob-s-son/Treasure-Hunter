5e2a648730809ceb00f0f384ff08154240c05898
Edgars Jekabsons <jekabsons.edgars@gmail.com>
2011-06-27 11:34:59 +0300

    New rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreasureHunter::Application.config.secret_token = '7983cf150a4444630a62004c8325647505ef4dceea554cb9f162a0fda6aa12caf7ba0d637e5f5787a40e6d32d3a95d3e8a9fb671e29d94659ac39c90cf8adbbd'
